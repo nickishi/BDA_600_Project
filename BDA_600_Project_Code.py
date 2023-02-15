fd2337e65e5693d565853cf7ae2f66f1724889b7
nickishi <mcish4@yahoo.com>
2023-02-14 17:15:50 -0800

    Update BDA_600_Project_Code.py

--- BDA_600_Project_Code.py
+++ BDA_600_Project_Code.py
@@ -10,7 +10,7 @@ from alpha_vantage.timeseries import TimeSeries
 def create_graphs(data, ticker):
     
     
-    df2["4. adjusted close"].plot()
+    data["5. adjusted close"].plot()
     plt.title("Stock Price of " + ticker + " from 2020 to end of 2022")
     plt.tight_layout()
     plt.grid()
@@ -23,26 +23,29 @@ def api_call(ticker):
     key = "4UGW7JB0G6QNPFPM"
 
     ts = TimeSeries(key, output_format = "pandas")
-    
-    comp_data , comp_meta_data = ts.get_daily_adjusted(symbol = ticker, outputsize='full')
-    print(comp_data)
-    fig = plt.figure(dpi = 80, facecolor = "w", edgecolor = "k")
-    print(comp_meta_data)
-    df2 = comp_data.loc["20200101" : "20221231"]
-    create_graphs(df2, ticker)
+    try:
+        comp_data , comp_meta_data = ts.get_daily_adjusted(symbol = ticker, outputsize='full')
+        print(comp_data)
+        fig = plt.figure(dpi = 80, facecolor = "w", edgecolor = "k")
+        print(comp_meta_data)
+        df2 = comp_data.loc["20200101" : "20221231"]
+        create_graphs(df2, ticker)
+    except:
+        print("Invalid Ticker: " + ticker)
+
     
 
     
 
 
 def main():
-    exit = "no"
-    while exit == "no":
+    exit = "yes"
+    while exit == "yes":
         ticker = input("Enter a ticker: ")
         ticker_upper = ticker.upper()
         data = api_call(ticker_upper)
-        create_graphs(data, ticker_upper)
-
+        #create_graphs(data, ticker_upper)
+        exit = input("Do you want to search another company (yes/no): ")
 
 
 
