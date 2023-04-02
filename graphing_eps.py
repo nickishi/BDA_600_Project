07987bf2612f67a35c74ff1ec8ed001e3c6ea6ed
nickishi <mcish4@yahoo.com>
2023-04-02 13:14:48 -0700

    stock_price_eps_graph function shows a graph of the stock data and vertical lines to designate eps date.

--- graphing_eps.py
+++ graphing_eps.py
@@ -12,12 +12,20 @@ import sys
 import os.path
 from datetime import datetime, timedelta, date
 
-def stock_price_percentage(eps_date_range, stock_date_range, stock_price_data):
+def stock_price_eps_graph(eps_date_range, stock_date_range, stock_price_data, eps_data):
     """WTF do I want this to do?
         - Calculate percent change before and after EPS report date?
         - Do I want to graph the EPS date and stock price? Possibly different function
         - """
-    for eps_date in eps_date_range:
+    plt.plot(stock_date_range, stock_price_data)
+
+
+    for date, eps in zip(eps_date_range, eps_data):
+        plt.axvline(x = date, color = 'b')
+
+    plt.xticks(rotation=45)
+
+    plt.show()
 
 
 def get_stock_price(ticker, date_range = None):
@@ -48,12 +56,13 @@ def get_stock_price(ticker, date_range = None):
         if start_date.strftime('%Y-%m-%d') <= stock_date <= end_date.strftime('%Y-%m-%d'):
 
             #temp_list.append(stock_date)
-            temp_list.append(value["5. adjusted close"])
-            date_range.append(stock_date)
+            needed_data_stock_price.append(float(value["5. adjusted close"]))
+            date_range.append(datetime.strptime(stock_date, '%Y-%m-%d'))
             print(stock_date, value["5. adjusted close"])
 
-        needed_data_stock_price.append(temp_list)
 
+    print(needed_data_stock_price)
+    print(date_range)
     return needed_data_stock_price, date_range
 
 
@@ -90,9 +99,9 @@ def get_eps(ticker):
 
         if start_date <= datetime.strptime(point['fiscalDateEnding'], '%Y-%m-%d') <= end_date:
             #temp_list.append(point['reportedDate'])
-            temp_list.append(point["surprisePercentage"])
-            needed_data.append(temp_list)
-            date_range.append(point['reportedDate'])
+            needed_data.append(point["surprisePercentage"])
+            #needed_data.append(temp_list)
+            date_range.append(datetime.strptime(point['reportedDate'], '%Y-%m-%d'))
     print(needed_data)
     print(date_range)
 
@@ -109,11 +118,11 @@ def main():
     stock_price_needed_data, stock_date_range = get_stock_price("AAPL")
     eps_needed_data, eps_date_range = get_eps("AAPL")
 
+    stock_price_eps_graph(eps_date_range, stock_date_range, stock_price_needed_data, eps_needed_data)
+    #for ticker in ticker_list:
 
-    for ticker in ticker_list:
-
-        stock_price_needed_data, stock_date_range = get_stock_price(ticker)
-        eps_needed_data, eps_date_range = get_eps(ticker)
+        #stock_price_needed_data, stock_date_range = get_stock_price(ticker)
+        #eps_needed_data, eps_date_range = get_eps(ticker)
 
 
 
