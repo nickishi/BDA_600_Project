9777010cfaeaecfd857c9e37bdee0d0a1a77e4d5
Marrion <mmacadog7304@sdsu.edu>
2023-04-07 16:43:06 -0700

    created csv for returns across all companies for time series analysis

--- mm_timeseries_R.R
+++ mm_timeseries_R.R
@@ -9,13 +9,13 @@ library(astsa)
 library(lmtest)
 library(TSA)
 
-
-data <- read.csv('cumulative_returns.csv')
+setwd("~/Desktop/PersonalProjects_Github/BDA_600_Project")
+data <- read.csv('cumulative_weekly_returns.csv')
 data$X = as.Date(data$X, tryFormat = "%Y-%m-%d")
 head(data)
 
 plot(data$X, data$layoff_group, type = 'l', xlab = 'Time', ylab = 'Return', main = 'Cumulative Return of Layoff Group 2020 - 2022')
-plot(data$X, data$no_layoff_group, type = 'l', xlab = 'Time', ylab = 'Return', main = 'Cumulative Return of Layoff Group 2020 - 2022')
+plot(data$X, data$no_layoff_group, type = 'l', xlab = 'Time', ylab = 'Return', main = 'Cumulative Return of NoLayoff Group 2020 - 2022')
 
 n = dim(data)[1]
 dates = as.Date(data$X[2:n], tryFormat = "%Y-%m-%d")
@@ -24,11 +24,13 @@ no_layoff_data = data$no_layoff_group
 
 # log to make stationary
 logx1 = log(layoff_data)
-dlogx1 = diff(log(layoff_data))
+dlogx1 = diff(logx1)
 
 plot(dates, dlogx1, type='l')
 adf.test(dlogx1) # P < .05, REGECT H0. data is stationary
 
-acf(dlogx1)
-pacf(dlogx1)
-
+par(mfrow = c(2, 1))
+acf(dlogx1) # white noise :-(
+pacf(dlogx1) # white noise :-(
+eacf(dlogx1)
+par(mfrow = c(1, 1))
